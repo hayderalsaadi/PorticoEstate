d7e59067c2604cca214eaed241b60933ccc7c089
Alsaadi <Hayder.Alsaadi@fjell.kommune.no>
2018-03-23 11:09:04 +0100

    initial commit

---
+++ phpgwapi/js/uploadify/uploadify-check-exists_v2.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Uploadify v3.0.0
+Copyright (c) 2010 Ronnie Garcia
+
+Return true if the file exists
+*/
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/uploads/' . $_POST['filename'])) {
+	echo 1;
+} else {
+	echo 0;
+}
